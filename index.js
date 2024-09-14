342eef3ed11f9ea7ed5525875ffd14717c1d4d1b
Sunil dharajiya <sunildharajiyablack@gmail.com>
2024-09-14 07:30:27 +0000

    update & push to github

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const pro = require('prompt-sync')();
 
 let randum = Math.random();
-let ran = randum*100;
+let ran = randum*1000;
 console.log(ran);
 let a = pro('enter value of a:');
 a= parseInt(a);
 let c = pro("enter a operation");
 let b = pro('enter value of b:');
 b = parseInt(b);
-if (a > 0 && a < 150){
+if (a < 100 && ran > 0){
 if (c == "+"){
   console.log((a), '+' ,(b) ,"=", (a+b))
 }else if (c == "-"){
